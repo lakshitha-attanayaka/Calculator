43f12e56727b30357e86cd20850faf1c74bf418f
DeshanFernando <dfernando.official@gmail.com>
2022-07-11 15:55:22 +0000

    Add project files.

---
+++ Calculator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		MainPage = new NavigationPage(new LoginUI());
+		
+		
+	}
+}
---
+++ Calculator/LoginUI.xaml.cs
@@ -0,0 +1,24 @@
+namespace Calculator;
+
+public partial class LoginUI : ContentPage
+{
+	public LoginUI()
+	{
+		InitializeComponent();
+	}
+
+
+	private void Btn_Clicked(object sender, EventArgs e)
+	{
+		if(Uname.Text=="User1" && Passwd.Text == "abc123")
+		{
+			DisplayAlert("Login", "Sucess", "OK");
+			App.Current.MainPage.Navigation.PushAsync(new MainPage());
+           // Navigation.PushAsync(new Home());
+		}
+		else
+		{
+			DisplayAlert("ALERT","Username or Password Wrong","OK");
+		}
+	}
+}
